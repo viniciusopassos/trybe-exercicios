ec42407f8492149ac737275275eeab18dfee0e90
viniciusOP <viniciusopassos@hotmail.com>
2021-11-05 16:23:30 -0300

    exercicio 3

--- fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
+++ fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
@@ -9,11 +9,24 @@
 //console.log(a % b);
 
 // ex2.
+//const a = 1;
+//const b = 2;
+
+//if (a > b){
+ //   console.log(a);
+//}else {
+  //  console.log(b);
+//}
+
+//ex3.
 const a = 1;
 const b = 2;
+const c = 3;
 
-if (a > b){
-    console.log(a);
+if (a > b && a > c){
+   console.log(a);
+}else if (a < b && b > c ) {
+  console.log(b);
 }else {
-    console.log(b);
+    console.log(c);
 }
