e8d07b70d082dd2788cbee7576b5be36f9831d7a
viniciusOP <viniciusopassos@hotmail.com>
2021-11-05 10:54:19 -0300

    exemplo tipos primitivos/tipagem dinamica/operadores aritmeticos

--- fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/Scripts.js
+++ fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/Scripts.js
@@ -1,9 +1,31 @@
 let age = 20;
 age = 30;
-let name = "Hulk";
+let Name = "Hulk";
 const peso = 90;
 
 
 console.log(age);
-console.log(name);
+console.log(Name);
 console.log(peso);
+
+// ------------------------------------------------------------------------- fim aula variáveis
+
+let movie = "avengers"; //string literal
+let score = 10.89; //number literal
+let isValid = true; //boolean
+let name; //underfined
+let color = null; //variável nula à espera de definição 
+
+//+, -, *, /, **
+let salary = 3500;
+salary++;
+salary--;
+
+console.log(salary+salary);
+console.log(salary-salary);
+console.log(salary*salary);
+console.log(salary/salary);
+console.log(2**2);
+
+//---------------------------- fim aula tipos primitivos, tipagem dinâmica, operadores aritméticos
+
---
+++ fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/ex-tipos-primitivos-tipagem-dinamica-operadores-aritmeticos.js
@@ -0,0 +1,17 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+const base = 5;
+let height = 8;
+const area = (base * height);
+const perimeter = (base + height);
+
+console.log(perimeter);
+console.log(base);
+console.log(height);
+console.log(area);
