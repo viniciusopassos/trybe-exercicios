e870184a953c0e332ecee8f42c244e56b368a69f
viniciusOP <viniciusopassos@hotmail.com>
2021-11-05 19:02:42 -0300

    exercicios 4 e 5

--- fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
+++ fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
@@ -19,9 +19,9 @@
 //}
 
 //ex3.
-const a = 1;
-const b = 2;
-const c = 3;
+/*const a = 3;
+const b = 23;
+const c = 1;
 
 if (a > b && a > c){
    console.log(a);
@@ -29,4 +29,26 @@ if (a > b && a > c){
   console.log(b);
 }else {
     console.log(c);
+}*/
+
+//ex4.
+/*const n = 5;
+
+if (n % 2 === 0){
+  console.log("positive");
+} else {
+  console.log("negative");
+}*/
+
+//ex5.
+
+const l1 = 60;
+const l2 = 50;
+const l3 = 60;
+
+if ((l1 + l2 + l3) === 180){
+  console.log("triângulo equilatero");
+} else {
+  console.log("valores inválidos para o triangulo equilatero");
 }
+
