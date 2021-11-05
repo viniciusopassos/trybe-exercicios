521063a54717c1b6897f790f2aa32df6988af698
viniciusOP <viniciusopassos@hotmail.com>
2021-11-05 19:22:46 -0300

    exercicio 6

--- fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
+++ fundamentos/bloco-4-JavaScript/dia-1-introducao-e-primeiros-passos/exercicio4.1.js
@@ -41,8 +41,7 @@ if (n % 2 === 0){
 }*/
 
 //ex5.
-
-const l1 = 60;
+/*const l1 = 60;
 const l2 = 50;
 const l3 = 60;
 
@@ -50,5 +49,21 @@ if ((l1 + l2 + l3) === 180){
   console.log("triângulo equilatero");
 } else {
   console.log("valores inválidos para o triangulo equilatero");
-}
+}*/
 
+//ex6. referências para fazer o exercicio https://blog.megajogos.com.br/conheca-as-pecas-do-xadrez/ https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+let pecaXadrez = "Rei";
+
+if (pecaXadrez.toLowerCase() === "rei"){
+  console.log("Pode ser movimentado em qualquer direção do tabuleiro, mas apenas de casa em casa.");
+} else if (pecaXadrez.toLowerCase() === "rainha"){
+  console.log("Sem restrições, a Rainha tem livre movimentação no jogo na horizontal, vertical e diagonais.");
+}else if (pecaXadrez.toLowerCase() === "torre"){
+  console.log("pode correr sem restrição de número de casas e em todas as direções (frente, trás, direita, esquerda).");
+}else if (pecaXadrez.toLowerCase() === "bispo"){
+  console.log("Sem restrição de número de casas, mas somente no sentido diagonal.");
+}else if (pecaXadrez.toLowerCase() === "cavalo"){
+  console.log("Realiza movimentos em “L”, ou seja, duas casas em um sentido e uma casa em sentido perpendicular àquele, em qualquer direção.");
+}else if (pecaXadrez.toLowerCase() === "peao"){
+  console.log("Pode apenas realizar movimentos frontais, de forma que o primeiro movimento de cada peão abranja até duas casas, e os demais se limitam a uma casa à frente. O ataque do peão sempre ocorre na diagonal.");
+}
