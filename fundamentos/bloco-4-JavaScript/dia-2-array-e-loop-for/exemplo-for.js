bbb4e54694d36d5534c482f7fe6e3011a8187fec
viniciusOP <viniciusopassos@hotmail.com>
2021-11-08 13:11:00 -0300

    exemplo e exercícios estrutura de repetição FOR

---
+++ fundamentos/bloco-4-JavaScript/dia-2-array-e-loop-for/exemplo-for.js
@@ -0,0 +1,14 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+// primeiro método
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+
+// segundo método
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
--- fundamentos/bloco-4-JavaScript/dia-2-array-e-loop-for/video-script.js
+++ fundamentos/bloco-4-JavaScript/dia-2-array-e-loop-for/video-script.js
@@ -6,7 +6,7 @@ let pizza3 = "frango";
 
 console.log(pizza1, pizza2, pizza3);*/
 
-/*arrays*/
+/*arrays
 let pizzas = ["4 queijos", "calabresa", "frango"];
 
 pizzas[4] = "marguerita";
@@ -19,4 +19,28 @@ console.log(pizzas[0]);
 
 for (let index = o; index < pizzas.length; index++){
     console.log(pizzas[index]);
+}
+----------------------------------------------------FIM AULA [ARRAYS] */
+
+var numero = 7;
+/*
+console.log(numero*1);
+console.log(numero*2);
+console.log(numero*3);
+console.log(numero*4);
+console.log(numero*5);
+console.log(numero*6);
+console.log(numero*7);
+console.log(numero*8);
+console.log(numero*9);*/
+
+for (var contador = 1; contador <= 9; contador++){
+    console.log(numero * contador);
+}
+
+var listaDeNomes = ["Joana", "Maria", "Lucas"];
+
+for (var indice = 0; indice < listaDeNomes.length; indice++){
+    var msg = "boas vindas. " + listaDeNomes[indice] + "!";
+    console.log(msg);
 }
