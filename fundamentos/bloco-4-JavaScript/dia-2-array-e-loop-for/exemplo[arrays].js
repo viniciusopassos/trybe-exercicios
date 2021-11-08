82e956975890e16be2f784ebce2ad0848b6f429f
viniciusOP <viniciusopassos@hotmail.com>
2021-11-08 12:48:46 -0300

    exemplo e exercícios [arrays],[listas]

---
+++ fundamentos/bloco-4-JavaScript/dia-2-array-e-loop-for/exemplo[arrays].js
@@ -0,0 +1,38 @@
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);*/
+// 3
+
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+/*let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);*/
+// Tomar café
+
+/*let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);*/
+// Brincar com o cachorro
+
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa no ínicio
+console.log(tasksList);*/
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.pop();  // remove a última tarefa
+console.log(tasksList);
+tasksList.shift();  // remove a primeira tarefa
+console.log(tasksList);*/
+
+// [ 'tomar café', 'Reunião']
+// [ 'Reunião' ]
+
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);*/
+
+// 1
---
+++ fundamentos/bloco-4-JavaScript/dia-2-array-e-loop-for/video-script.js
@@ -0,0 +1,22 @@
+/*let variavel = "valor";*/
+
+/*let pizza1 = "4 queijos";
+let pizza2 = "calabresa";
+let pizza3 = "frango";
+
+console.log(pizza1, pizza2, pizza3);*/
+
+/*arrays*/
+let pizzas = ["4 queijos", "calabresa", "frango"];
+
+pizzas[4] = "marguerita";
+pizzas.push("presunto");
+
+console.log(pizzas.length);
+console.log(pizzas);
+console.log(pizzas.sort());
+console.log(pizzas[0]);
+
+for (let index = o; index < pizzas.length; index++){
+    console.log(pizzas[index]);
+}
