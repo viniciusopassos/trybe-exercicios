45732ec78caee4e5a4401c0781bd6723e2bc7c0a
viniciusopassos <viniciusopassos@hotmail.com>
2023-07-10 17:44:42 -0300

    feat: conteúdo 01-introdução-à-Python/dia-03

---
+++ ciência-da-computação/01-introdução-à-Python/03-testes/03-um_pouco_de_contexto/codigo.py
@@ -0,0 +1,25 @@
+def get_most_ordered_dish_per_costumer(orders, customer):
+    max_amount = 0
+    most_ordered = ""
+    customer_dishes = {}
+
+    for order in orders:
+        if order["customer"] == customer:
+            customer_dishes[order["order"]] = (
+                customer_dishes.get(order["order"], 0) + 1
+            )
+            if customer_dishes[order["order"]] >= max_amount:
+                max_amount = customer_dishes[order["order"]]
+                most_ordered = order["order"]
+    return most_ordered
+
+
+def get_order_frequency_per_costumer(orders, customer, order):
+    counter = 0
+    for current_order in orders:
+        if (
+            current_order["customer"] == customer
+            and current_order["order"] == order
+        ):
+            counter += 1
+    return counter
